bc3d47b7d176657215a95802c998d67bc8365468
1500182 <1500182@01HW795718.India.TCS.com>
2019-08-20 18:24:53 +0530

    Final Change For JSON Validator

--- JSON/src/com/DeepestLevelJSON2.java
+++ JSON/src/com/DeepestLevelJSON2.java
@@ -1,14 +1,20 @@
 package com;
 
-public class DeepestLevelJSON {
+import java.util.Stack;
+
+public class DeepestLevelJSON2 {
 	
 	public static void main(String[] args) {
 		
-		String input= "{\"name\": [\"1\",\"2\"}]";
+		//String input= "[{}]";
+		//String input= "{[]";
+		//String input= "[] {}";
+		//String input= "{\"name\": [\"1\",\"2\"}]";
 		//String input= "{\"name\":\"John\",\"age\":30,\"cars\":[{\"name\":\"Ford\",\"models\":[\"Fiesta\",\"Focus\",\"Mustang\"]},{\"name\":\"BMW\",\"models\":[\"320\",\"X3\",\"X5\"]},{\"name\":\"Fiat\",\"models\":[\"500\",\"Panda\"]}]}";
 		//String input= "[{\"name\":\"John\",\"age\":30,\"cars\":[{\"name\":\"Ford\",\"models\":[\"Fiesta\",\"Focus\",\"Mustang\"]},{\"name\":\"BMW\",\"models\":[\"320\",\"X3\",\"X5\"]},{\"name\":\"Fiat\",\"models\":[\"500\",\"Panda\"]}]},{\"name\":\"John\",\"age\":30,\"cars\":[{\"name\":\"Ford\",\"models\":[\"Fiesta\",\"Focus\",\"Mustang\"]},{\"name\":\"BMW\",\"models\":[\"320\",\"X3\",\"X5\"]},{\"name\":\"Fiat\",\"models\":[\"500\",\"Panda\"]}]}]";
 		//String input= "{\"b\": {\"name\": {\"fName\": \"Hii\", \"lName\": \"Hello\"} }, \"a\": {\"name\": \"John\", \"age\": \"10\"} }";
 		//String input= "[[[[]]]]";
+		String input= "{\"a\": {\"name\": \"John\", \"age\": \"10\"} }";
 		input= input.replace("\"", "");
 		input= input.replace(" ", "");
 		
@@ -20,18 +26,54 @@ public class DeepestLevelJSON {
 	}
 	
 	static int findMaxDepth(String input) {
+		
+		char firstChar= input.charAt(0);
+		
+		if(firstChar == '{') {
+			int lastIndex= input.lastIndexOf("}");
+			String newString= input.substring(lastIndex+1);
+			if(newString.length() > 0) {
+				if(newString.indexOf('{') != -1 || newString.indexOf('[') != -1){
+					throw new RuntimeException("Invalid JSON");
+				}
+			}
+		}else if(firstChar == '[') {
+			int lastIndex= input.lastIndexOf("]");
+			String newString= input.substring(lastIndex+1);
+			if(newString.length() > 0) {
+				if(input.indexOf('{') != -1 || input.indexOf('[') != -1){
+					throw new RuntimeException("Invalid JSON");
+				}
+			}
+		}
+		
 		int max= 0;
 		int currentMax= 0;
 		int length= input.length();
 		
+		Stack<Character> stack= new Stack<>();
+		
 		for(int i=0; i< length; i++) {
-			if(input.charAt(i) == '{' || input.charAt(i) == '[') {
+			
+			char currentChar= input.charAt(i);
+			
+			if(currentChar == '{') {
 				currentMax++;
-				
 				if(currentMax > max) {
 					max= currentMax;
 				}
-			}else if(input.charAt(i) == '}' || input.charAt(i) == ']') {
+				stack.push('}');
+			}else if(currentChar == '[') {
+				currentMax++;
+				if(currentMax > max) {
+					max= currentMax;
+				}
+				stack.push(']');
+			}else if(currentChar == '}'|| currentChar == ']') {
+				if(stack.isEmpty() || currentChar != stack.peek()) {
+					throw new RuntimeException("Invalid JSON");
+				}
+				stack.pop();
 				if(currentMax > 0) {
 					currentMax--;
 				}else {
